9a27ce7b046b20dd6f6a028a60648073580b8e40
Peter Connell <peterconnellit@gmail.com>
2023-03-30 21:47:40 +0100

    build: movies, review, movie id and login routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,20 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function App(){
+
+  /*hook to add local state to component, useState declares state variable with null as initial user value.
+  React preserves state between re-renders. useState returns array with two values: current state and a function to update,
+  current state user value to user, and the function to update it to setUser */
+  const [user, setUser] = React.useState(null)
+
+  async function login(user = null) {// default user to null
+    setUser(user)
+  }
+
+  async function logout(){
+    setUser(null)
+  }
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
@@ -25,7 +39,7 @@ function App(){
             <Nav.Link>
               {/* Conditional rendering. Login or out depending on state using ternary statement */}
               { user ? (
-                <a>Logout User</a>
+                <a onClick={logout}>Logout User</a>
               ) : (
                 <Link to={"/login"}>Login</Link>
               )}
@@ -33,6 +47,28 @@ function App(){
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+
+      {/* switch component switches between routes, rendering the first that matches.
+      Render allows props to be passed into a component rendered by React Router.
+      Here we are passing user as props to the Addreview component*/}
+      <switch>
+        <Route exact path={["/", "/movies"]} component={MoviesList}>          
+        </Route>
+        <Route path="/movies/:id/review" render={(props)=>
+          <AddReview {...props} user={user} />
+        }>          
+        </Route>
+        {/* routes for a specific movie and login to render components */}
+        <Route path="/movies/:id/" render={(props)=>
+          <Movie {...props} user={user} />
+        }>          
+        </Route>
+        <Route path="/login" render={(props)=>
+        /* passes login function as a prop */
+          <Login {...props} login={login} />
+        }>          
+        </Route>
+      </switch>
     </div>
   );
 }
